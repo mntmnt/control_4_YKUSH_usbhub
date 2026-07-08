3270b726eb17178b032d5de631f45f7b3a7e569a
Dmitry DXM <mntmnt7@gmail.com>
2026-07-08 22:58:43 +0300

    minor: improve formatting

--- usbswitch/usb-upstream-switch.cpp
+++ usbswitch/usb-upstream-switch.cpp
@@ -13,6 +13,7 @@ USBUpstreamSwitch::USBUpstreamSwitch(QObject * parent):
     this->connector = new details::Connector;
     this->connector->moveToThread(this);
     connect(this, &QThread::finished, connector, &details::Connector::deleteLater);
+
     connect(connector, &details::Connector::connected,  this, &USBUpstreamSwitch::deviceConnected);
     connect(connector, &details::Connector::connected,  this, &USBUpstreamSwitch::deviceInfoChanged);
     connect(connector, &details::Connector::twoDevError,this, &USBUpstreamSwitch::errorTooMuch);
@@ -20,12 +21,13 @@ USBUpstreamSwitch::USBUpstreamSwitch(QObject * parent):
     interactor = new  details::Interactor;
     interactor->moveToThread(this);
     connect(this, &QThread::finished, interactor, & details::Interactor::deleteLater);
-    connect(connector,  & details::Connector::connected, interactor, &details::Interactor::start);
-    connect(interactor, & details::Interactor::disconnected, connector, &details::Connector::wait4device);
-    connect(interactor, & details::Interactor::disconnected, this, &USBUpstreamSwitch::deviceDisconnected);
 
-    connect(interactor, & details::Interactor::portStateUpdated, this, &USBUpstreamSwitch::portStateUpdated);
-    connect(interactor, & details::Interactor::error,            this, &USBUpstreamSwitch::errorReported);
+    connect(connector,  &details::Connector::connected,     interactor, &details::Interactor::start);
+    connect(interactor, &details::Interactor::disconnected, connector,  &details::Connector::wait4device);
+    connect(interactor, &details::Interactor::disconnected, this,       &USBUpstreamSwitch::deviceDisconnected);
+
+    connect(interactor, &details::Interactor::portStateUpdated, this, &USBUpstreamSwitch::portStateUpdated);
+    connect(interactor, &details::Interactor::error,            this, &USBUpstreamSwitch::errorReported);
 
     QTimer::singleShot(100ms, connector, &details::Connector::wait4device);
     start();
